9a45cad56e4cbc578517038a05c91f958e561ebf
Vincent Lainé <v.laine@dental-monitoring.com>
2023-06-21 08:03:48 +0200

    Fix

--- ToDoAppPremium/Controllers/HomeController.cs
+++ ToDoAppPremium/Controllers/HomeController.cs
@@ -45,7 +45,7 @@ public class HomeController : Controller
             var day = random.Next(1, 15);
             string fromDate = $"06/{day}/2023";
             var response = await this.toDoService.GetToDosAsync(fromDate);
-            this._logger.LogDebug("End GetToDosAsync");
+            this._logger.LogInformation("End GetToDosAsync");
             return response;
         }
         catch (Exception ex)
